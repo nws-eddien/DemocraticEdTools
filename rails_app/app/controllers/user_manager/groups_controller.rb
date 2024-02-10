b06e444c7fac8470436d4ee73832b3c7540f2cb8
eddien <eddien@eddien.de>
2024-02-10 03:03:20 +0100

    Groups rewritten

--- rails_app/app/controllers/user_manager/groups_controller.rb
+++ rails_app/app/controllers/user_manager/groups_controller.rb
@@ -1,71 +1,59 @@
-#docker exec -it dst_rails_dev rails g scaffold_controller user_manager::group name --model-name="Group"
 class UserManager::GroupsController < ApplicationController
-  before_action :set_group, only: %i[ show edit update destroy ]
+  before_action :set_group, only: [:show, :edit, :update, :destroy]
 
-  # GET /user_manager/groups or /user_manager/groups.json
   def index
-    @groups = Group.all
+    @groups = Group.ordered
   end
 
-  # GET /user_manager/groups/1 or /user_manager/groups/1.json
   def show
   end
 
-  # GET /user_manager/groups/new
   def new
     @group = Group.new
   end
 
-  # GET /user_manager/groups/1/edit
-  def edit
-  end
-
-  # POST /user_manager/groups or /user_manager/groups.json
   def create
     @group = Group.new(group_params)
 
-    respond_to do |format|
-      if @group.save
-        format.html { redirect_to user_manager_group_url(@group), notice: "Group was successfully created." }
-        format.json { render :show, status: :created, location: @group }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
+    if @group.save
+      respond_to do |format|
+        format.html  { redirect_to user_manager_groups_path, notice: "Group was successfully created." }
+        format.turbo_stream { flash.now[:notice] = "Group was successfully created." }
       end
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /user_manager/groups/1 or /user_manager/groups/1.json
+  def edit
+  end
+
   def update
-    respond_to do |format|
-      if @group.update(group_params)
-        format.html { redirect_to user_manager_group_url(@group), notice: "Group was successfully updated." }
-        format.json { render :show, status: :ok, location: @group }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
+    if @group.update(group_params)
+      respond_to do |format|
+        format.html { redirect_to user_manager_groups_path, notice: "Group was successfully updated." }
+        format.turbo_stream { flash.now[:notice] = "Group was successfully updated." }
       end
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /user_manager/groups/1 or /user_manager/groups/1.json
   def destroy
-    @group.destroy!
-
+    @group.destroy
     respond_to do |format|
-      format.html { redirect_to user_manager_groups_url, notice: "Group was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to user_manager_groups_path, notice: 'Gruppe wurde gelöscht!' }
+      format.turbo_stream { flash.now[:notice] = "Gruppe wurde gelöscht!" }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_group
-      @group = Group.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def group_params
-      params.require(:group).permit(:name)
-    end
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
---
+++ rails_app/app/controllers/user_manager_old/groups_controller.rb
@@ -0,0 +1,66 @@
+#docker exec -it dst_rails_dev rails g scaffold_controller user_manager::group name --model-name="Group"
+class UserManager::GroupsController < ApplicationController
+  before_action :set_group, only: %i[ show edit update destroy ]
+
+  # GET /user_manager/groups or /user_manager/groups.json
+  def index
+    @groups = Group.all
+  end
+
+  # GET /user_manager/groups/1 or /user_manager/groups/1.json
+  def show
+  end
+
+  # GET /user_manager/groups/new
+  def new
+    @group = Group.new
+  end
+
+  # GET /user_manager/groups/1/edit
+  def edit
+  end
+
+  # POST /user_manager/groups or /user_manager/groups.json
+  def create
+    @group = Group.new(group_params)
+
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to user_manager_group_url(@group), notice: "Group was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /user_manager/groups/1 or /user_manager/groups/1.json
+  def update
+    respond_to do |format|
+      if @group.update(group_params)
+        format.html { redirect_to user_manager_group_url(@group), notice: "Group was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /user_manager/groups/1 or /user_manager/groups/1.json
+  def destroy
+    @group.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to user_manager_groups_url, notice: "Group was successfully destroyed." }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_group
+      @group = Group.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def group_params
+      params.require(:group).permit(:name)
+    end
+end

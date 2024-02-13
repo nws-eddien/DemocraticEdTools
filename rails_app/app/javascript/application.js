0acd81a5c9e72f08053dd91f1a40093d0346dc10
eddien <eddien@eddien.de>
2024-02-14 00:18:53 +0100

    Custom Turbo Confirm

--- rails_app/app/javascript/application.js
+++ rails_app/app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+Turbo.setConfirmMethod((message, element) => {
+    console.log(message, element)
+    let dialog = document.getElementById("turbo-confirm")
+    dialog.querySelector("p").textContent = message
+    dialog.showModal()
+
+    return new Promise((resolve, reject) => {
+        dialog.addEventListener("close", () => {
+            resolve(dialog.returnValue == "confirm")
+        }, { once: true })
+    })
+})
